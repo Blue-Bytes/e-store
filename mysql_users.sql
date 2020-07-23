17910a8830f143648f222a738634be0104eb9293
Blue-Bytes <67641370+Blue-Bytes@users.noreply.github.com>
2020-07-23 08:03:59 +0100

    Update mysql_users.sql
    because we changed the names of the tables ( update db.sql) we needed to make changes here

--- mysql_users.sql
+++ mysql_users.sql
@@ -5,10 +5,10 @@
 */ 
 CREATE USER 'store_admin'@'%' IDENTIFIED BY 'admin';
 GRANT SELECT, INSERT, UPDATE, DELETE ON `store`.`client` TO 'store_admin'@'%';
-GRANT SELECT, INSERT, UPDATE, DELETE ON `store`.`product` TO 'store_admin'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `store`.`article` TO 'store_admin'@'%';
 
 CREATE USER 'store_client'@'%' IDENTIFIED BY 'client';
 GRANT SELECT, INSERT, DELETE ON `store`.`ligne` TO 'store_client'@'%';
 GRANT SELECT, INSERT, DELETE ON `store`.`commande` TO 'store_client'@'%';
-GRANT SELECT ON `store`.`product` TO 'store_client'@'%';
+GRANT SELECT ON `store`.`article` TO 'store_client'@'%';
 GRANT SELECT ON `store`.`client` TO 'store_client'@'%';
