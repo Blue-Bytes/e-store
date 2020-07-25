6fb10f6d0322560f97cda91cc85ec6da75cc3a65
root <bluebytes88@gmail.com>
2020-07-25 03:08:07 +0100

    commit

--- admin/controllers/sign_out.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['admin']);
-header('Location: ../views/sign_in.php');
--- admin/views/header.php
+++
@@ -1,8 +0,0 @@
-<header>
-	<div id="logo">
-		<a href="home.php"><img src="../images/logo.png" alt="logo" />Warehouse</a>
-	</div>
-	<div id="sign_out">
-		<a href="../controllers/sign_out.php">Sign out</a>
-	</div>
-</header>
