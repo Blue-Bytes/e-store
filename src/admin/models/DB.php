ad8c17aa3c6f495e64ec01751c7d8e2fc16347b7
marouane <marouane.ourdou@edu.uiz.ac.ma>
2020-07-25 04:11:57 +0100

    not important

--- src/admin/models/DB.php
+++ src/admin/models/DB.php
@@ -8,12 +8,7 @@ class DB
     public static function connect($user, $password) {
         $db='mysql:host=localhost;dbname=store';
         try {
-<<<<<<< HEAD
             $connection = new PDO($db, $user, $password);
-=======
-            $connection=new PDO($db, $user, $password);
-            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
->>>>>>> 6fb10f6d0322560f97cda91cc85ec6da75cc3a65
         }
         catch (PDOException $e) {
             echo $e->getMessage();
